137386c1813c2c38208ada0f38ab4aef3f221baa
Saam Koukpari <saam.koukpari@gmail.com>
2021-09-23 16:11:54 -0700

    refactored to add testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,18 @@
 const request = require('request');
+const fetchBreedDescription = function(breedName, callback) {
 
-let searchTerm = process.argv[2]
-request(`https://api.thecatapi.com/v1/breeds/search?q=${searchTerm}`, function (error, response, body) {
-  if (error) {
-    return console.log('Request Error')
-  }
-let parsed = JSON.parse(body)
-let breed = parsed[0]
-  if (!breed) {
-    return console.log('Sorry, breed not found!')
-  }
-  console.error('error:', error); 
-  console.log('statusCode:', response && response.statusCode); 
-  console.log('body:', breed.description); 
-});
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(Error, null);
+    }
+    const data = JSON.parse(body);
+    const breed = data[0];
+    if (!breed) {
+      return callback(Error, null);
+    }
+    callback(null, breed.description);
+  });
+};
+
+module.exports = { fetchBreedDescription };
+
